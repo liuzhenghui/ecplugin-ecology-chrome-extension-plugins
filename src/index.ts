329e1fe7e092a6af12002a2dc793f2293d4274e0
liuzhenghui <liu.zhenghui@outlook.com>
2024-11-29 00:29:27 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import './popup-menus'
 
-console.log('kkkkkkkkkkkkkkkkkkkkk')
+console.log('bbbbbb')
 
 
 // cdn: https://fastly.jsdelivr.net/gh/liuzhenghui/ecplugin-ecology-chrome-extension-plugins@master/release/bundle.js?t=
